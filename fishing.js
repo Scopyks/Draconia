b6800c265a0ccecb1ab557f4c593833744824852
Scopyks <lillie62430@hotmail.fr>
2026-09-14 11:37:53 +0200

    Load exploration zones extension

--- fishing.js
+++ fishing.js
@@ -211,6 +211,7 @@ injectFishingUpgradeStyles();
     loadScriptOnce("hunting.js?v=2", "draconiaHunting");
     loadScriptOnce("cooking.js?v=1", "draconiaCooking");
     loadScriptOnce("missions.js?v=2", "draconiaMissions");
+    loadScriptOnce("exploration.js?v=1", "draconiaExploration");
 
     loadScriptOnce(
         "dragoncare.js?v=1",
