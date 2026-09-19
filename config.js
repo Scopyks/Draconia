e3d991a16f4cc1062ddaa2c123adef10fb26b2d2
Scopyks <lillie62430@hotmail.fr>
2026-09-19 11:51:32 +0200

    Fix Supabase publishable API key

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
         // À compléter avec les deux valeurs PUBLIQUES du projet Supabase.
         cloud: {
             supabaseUrl: "https://qaucukmxomlgumoryxxh.supabase.co",
-            supabaseAnonKey: "sb_publishable_WyAN-yBGBVfwylB6sGGJFwu_u7f-8-tr"
+            supabaseAnonKey: "sb_publishable_WyAN-yBGBVfxylB6sGGJFw_u7f-8-tr"
         },
 
         storage: {
